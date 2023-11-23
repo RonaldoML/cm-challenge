d56388eb25516f974f3468e17212fb2dc97e15fd
Ronaldo Monserrate <ronsml1594@gmail.com>
2023-11-22 22:24:02 -0500

    Not found page

---
+++ src/pages/NotFound.tsx
@@ -0,0 +1,8 @@
+
+export const NotFound = () => {
+  return (
+    <section className="d-flex align-items-center justify-content-center vh-100" >
+      <p className="align-self-center">404 | Page not found</p>
+    </section>
+  );
+};
